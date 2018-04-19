ba9dc5f974c0d88cde5adbdeaffb9b0b508225b0
Daniel Lande <danlande@gmail.com>
2018-04-19 17:13:59 -0600

    Something

--- app/src/main/java/com/kirklandtech/toiletries/MainActivity.java
+++ app/src/main/java/com/kirklandtech/toiletries/MainActivity.java
@@ -16,16 +16,16 @@ import android.view.MenuItem;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button loginButton;
+    private Button loginButtosdfsdfsdfn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        loginButton = findViewById(R.id.login_button);
+        loginButtosdfsdfsdfn = findViewById(R.id.login_button);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        loginButtosdfsdfsdfn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), LoggedInActivity.class);
