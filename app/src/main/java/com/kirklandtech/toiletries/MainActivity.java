75382d55748a4c0ea96df7215d54e9b2d1e0460a
Daniel Lande <danlande@gmail.com>
2018-04-19 17:12:26 -0600

    Renamed all navigation drawer files

--- app/src/main/java/com/kirklandtech/toiletries/MainActivity.java
+++ app/src/main/java/com/kirklandtech/toiletries/MainActivity.java
@@ -16,16 +16,16 @@ import android.view.MenuItem;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button loginsaksdfsadfasdfButton;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // asldkfj;laskjdf
-        loginsaksdfsadfasdfButton = findViewById(R.id.login_button);
 
-        loginsaksdfsadfasdfButton.setOnClickListener(new View.OnClickListener() {
+        loginButton = findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), LoggedInActivity.class);
