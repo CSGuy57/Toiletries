f6aad5fdf07eac908eeac1af2a320afce9dcc114
Kirkland C <kirklandsolar@gmail.com>
2018-05-01 17:37:46 -0600

    Finished the signup and login worksheet

--- app/src/main/java/com/kirklandtech/toiletries/LoginActivity.java
+++ app/src/main/java/com/kirklandtech/toiletries/LoginActivity.java
@@ -1,9 +1,9 @@
 // LoginActivity.java
 package com.kirklandtech.toiletries;
 
-import android.support.v7.app.AppCompatActivity;    //
-import android.os.Bundle;                           //
-import android.view.View;                           //
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
