7278176fb17ebb7b19d8dec7708953096216ada0
Kirkland C <kirklandsolar@gmail.com>
2018-05-09 17:16:03 -0600

    Test

--- app/src/main/java/com/kirklandtech/toiletries/MainActivity.java
+++ app/src/main/java/com/kirklandtech/toiletries/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private final String TAG = MainActivity.class.toString();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) { 
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
